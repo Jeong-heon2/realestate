3c390f5bfd1ea2efc2bbb64eb15aec0e68fe6403
Jeong-heon2 <cjhgg2@gmail.com>
2020-11-24 03:33:57 +0900

    지도의 중심 좌표를 다대포로 이동

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ $list = '';
 <script>
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         mapOption = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-            level: 10 // 지도의 확대 레벨
+            center: new kakao.maps.LatLng(35.0510087, 128.9677272), // 지도의 중심좌표
+            level: 6 // 지도의 확대 레벨
         };
 
     var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
